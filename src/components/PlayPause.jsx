b349621766d7f2e37200ca339dc173398a8a7631
Emeldalaeticia <114027865+Emeldalaeticia@users.noreply.github.com>
2023-08-11 22:39:15 +0300

    Top Charts added

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,10 +1,10 @@
 import {FaPauseCircle, FaPlayCircle} from 'react-icons/fa';
 
-const PlayPause = ({isPlaying, activeSong, song, handelPause, handlePlay}) => ( isPlaying && activeSong?.title === song.title ? (
+const PlayPause = ({isPlaying, activeSong, song, handlePause, handlePlay}) => ( isPlaying && activeSong?.title === song.title ? (
   <FaPauseCircle
     size={35}
     className='text-gray-300'
-    onClick={handelPause}
+    onClick={handlePause}
   
   />
 ) : (
--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,12 +1,13 @@
+/* eslint-disable react/prop-types */
 import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
-
+// import { FreeMode } from "swiper";
 
 
 import PlayPause from './PlayPause';
-import {setActiveSong, playPause} from '../redux/features/playerSlice';
+import { playPause, setActiveSong} from '../redux/features/playerSlice';
 import {useGetTopChartsQuery} from '../redux/services/shazamCore';
 
 
@@ -14,10 +15,29 @@ import 'swiper/css';
 import 'swiper/css/free-mode'
 
 
-const TopChartCard = ( song, i ) => (
+const TopChartCard = ( { song, i, isPlaying, activeSong, handlePauseClick, handlePlayClick } )  => (
   <div className="w-full flex flex-row items-center hover:bg[#FFFFFF] py-2 p-4 rounded-lg cursor-pointer mb-2">
+    <h3 className="fot-bold text-base text-white mr-3">{i+1}.</h3>
+    <div className="flex-1 flex flex-row justify-between items-center">
+      <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
 
-    {song.title}
+      <div className="flex-1 flex-col justify-center mx-3">
+
+        <Link to={`/songs/${song.key}`}>
+          <p className="text-xl font-bold text-white"> {song?.title} </p>
+        </Link>
+        <Link to={`/artists/${song?.artists[0].adamid}`}>
+          <p className="text-ase text-gray-300 mt-1"> {song?.subtitle} </p>
+        </Link>    
+      </div> 
+    </div>
+    <PlayPause
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      song={song}
+      handlePauseClick={handlePauseClick}
+      handlePlayClick={handlePlayClick}
+    />
   </div>
 )
 
@@ -34,17 +54,18 @@ const TopPlay = () => {
   });
 
   const topPlays = data?.slice(0, 5);
+
   const handlePauseClick = () => {
     dispatch(playPause(false));
 
   };
 
-  const handlePlayClick = () => {
+  const handlePlayClick = (song, i ) => {
       dispatch(setActiveSong({ song, data, i }));
       dispatch(playPause(true));
 
   };
-  
+
   return (
     <>
     <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
@@ -62,6 +83,10 @@ const TopPlay = () => {
               key={song.key}
               song={song}
               i={i}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayClick={ () => handlePlayClick(song, i)}
             />
           ))}
 
@@ -74,6 +99,29 @@ const TopPlay = () => {
               <p className="text-gray-300 text-base cursor-pointer">See More</p>
             </Link>
       </div>
+
+      <Swiper
+      slidesPerView="auto"
+      spaceBetween={15}
+      freeMode={true}
+      centeredSlides
+      centeredSlidesBounds
+      className="mt-4"
+      >
+        {topPlays?.map((song, i) => (
+          <SwiperSlide 
+          key={song?.key}
+          style={{ width: '25%', height: 'auto'}}
+          className="shadow-lg rounded-full animate-slideright"
+          >
+            <Link to={`./artists/${song?.artists[0].adamid}`}>
+              <img src={song?.images.background} alt="name"
+              className="rounded-full w-full object-cover"/>
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
     </div>
     
     </>
