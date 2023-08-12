674165741faf69d2d973079ce8d9d262b4092af8
Emeldalaeticia <114027865+Emeldalaeticia@users.noreply.github.com>
2023-08-12 22:38:07 +0300

    Updated the song details when clicked on

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/jsx-key */
+
 import { useParams } from "react-router-dom";
 import {  useDispatch, useSelector } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components'
@@ -18,7 +18,7 @@ const SongDetails = () => {
 
 return (
     <div className=" flex flex-col">
-        {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
+        <DetailsHeader artistId="" songData={songData} />
 
         <div className="mb-10">
             <h2 className="text-white text-3xl font-bold">
@@ -26,10 +26,10 @@ return (
 
             <div className="mt-5">
                 {songData?.sections[1].type === 'LYRICS' ? songData?.sections[1].text.map((line, i) => (
-                    <p className="text-gray-400 text-base my-1">{line}</p>
+                    <p key={i} className="text-gray-400 text-base my-1">{line}</p>
                 )) : <p className="text-gray-400 text-base my-1"> Sorry, no lyrics found!</p>}
-
             </div>
+
 
         </div>
 
