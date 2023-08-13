fed3efb80a777c2f8a963f99c3e248ed1879fbd3
Emeldalaeticia <114027865+Emeldalaeticia@users.noreply.github.com>
2023-08-13 04:12:32 +0300

    Updated the discover page to show music by genre

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -25,6 +25,7 @@ export const shazamCoreApi = createApi({
     }),
     endpoints: (builder) => ({
         getTopCharts: builder.query({query: () => '/charts/world'}),
+        getSongsByGenre: builder.query({ query: ( genre ) => `/charts/genre-world?genre_code=${genre}` }),
         getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
         getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}`}),
         getArtistDetails: builder.query({ query: (artistid ) => `/artists/details?artistid=${artistid}`}),
@@ -35,6 +36,7 @@ export const shazamCoreApi = createApi({
 
 export const {
     useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
     useGetArtistDetailsQuery,
